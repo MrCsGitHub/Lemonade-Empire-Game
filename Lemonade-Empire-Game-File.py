26d2214b129939341da30362083f878a550952e9
MrCsGitHub <39196076+MrCsGitHub@users.noreply.github.com>
2018-07-18 15:50:51 -0500

    Added the better structure branch.
    Trying to build a better structure to expand the code.

--- Lemonade-Empire-Game-File.py
+++ Lemonade-Empire-Game-File.py
@@ -5,22 +5,29 @@ from time import sleep
 lemonade = 0
 money = 10
 current_time = 0
+day = 0
 
 
 #functions
-def buy_lemonade():
+def display_stats():
+    print("")
+    print("Day: "+str(day))
+    print("Cups of Lemonade: "+str(lemonade))
+    print("Money: "+str(money))
+    print("You have %s hours remaining in the day." % str(round(8 - current_time, 1)))
+    print("")
+
+def buy_lemonade(amount):
     global lemonade
     global money
     global current_time
-    if money >= 3:
-        lemonade += 10
-        money -= 3
+    global day
+    if money >= 3 * amount:
+        lemonade += 10 * amount
+        money -= 3 *amount
+        sleep(1)
         current_time += 1
-        print("")
-        print("Cups of Lemonade: "+str(lemonade))
-        print("Money left: "+str(money))
-        print("You have %s hours remaining in the day." % str(round(8 - current_time, 1)))
-        print("")
+        display_stats()
     else:
         print("")
         print("You don't have enough money!")
@@ -30,6 +37,7 @@ def sell_lemonade():
     global lemonade
     global money
     global current_time
+    global day
     if lemonade > 0:
         sell_time = randint(1, 10)/10
         sell_time = round(sell_time, 1)
@@ -38,15 +46,19 @@ def sell_lemonade():
         current_time = round(current_time, 1)
         lemonade -= 1
         money += 1
-        print("")
-        print("Cups of Lemonade: "+str(lemonade))
-        print("Money: "+str(money))
-        print("You have %s hours remaining in the day." % str(round(8 - current_time, 1)))
-        print("")
+        display_stats()
     else:
         print("")
         print("You don't have any lemonade!")
         print("")
+
+def check_time():
+    global current_time
+    global day
+    if current_time >= 8:
+        print("Day is done!")
+        current_time = 0
+        day += 1
 
 
 #main game loop
@@ -56,23 +68,30 @@ while True:
 
     if command == "help":
         print('''
-        buy_lemonade -> buys 10 cups of lemonade for the price of 3 dollars
+        buy_lemonade -> buys 10 cups/1 bottle of lemonade for the price of 3 dollars
         sell_lemonade -> sells lemonade for 1 dollar a cup
         display_stats -> displays how much money, lemonade, etc. you have
         exit -> quits the game
         ''')
     if command == "buy_lemonade":
-        buy_lemonade()
+        buy_amount = input("How many bottles?(1 bottle = 10 cups): ")
+        try:
+            buy_amount = int(buy_amount)
+            buy_lemonade(buy_amount)
+        except:
+            print("Enter a number next time!")
     if command == "sell_lemonade":
-        sell_lemonade()
+        sell_amount = input("How much do you want to sell?: ")
+        try:
+            sell_amount = int(sell_amount)
+            for i in range(sell_amount):
+                sell_lemonade()
+                check_time()
+            print("Done")
+        except:
+            print("Enter a number next time!")
     if command == "exit":
         break
     if command == "display_stats":
-        print("")
-        print("Cups of Lemonade: "+str(lemonade))
-        print("Money: "+str(money))
-        print("You have %s hours remaining in the day." % str(round(8 - current_time, 1)))
-        print("")
-    if current_time > 8:
-        print("Day is done!")
-        current_time = 0
+        display_stats()
+    check_time()
