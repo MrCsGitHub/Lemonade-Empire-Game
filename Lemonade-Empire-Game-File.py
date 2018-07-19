d42a3ad958edf69cd4a92af080ace9ab939e816c
MrCsGitHub <39196076+MrCsGitHub@users.noreply.github.com>
2018-07-19 11:00:33 -0500

    Update Lemonade-Empire-Game-File.py

--- Lemonade-Empire-Game-File.py
+++ Lemonade-Empire-Game-File.py
@@ -1,68 +1,64 @@
-#imports
+#dependencies
 from random import randint
 from time import sleep
 #variables
-lemonade = 0
 money = 10
-current_time = 0
-day = 0
 
 
-#functions
+#classes
+class time:
+    def __init__(self, current_time, day):
+        self.day = day
+        self.current_time = current_time
+    def tick(self):
+        self.current_time -= 1
+        if self.current_time == 0:
+            self.day += 1
+            self.current_time = 8
+    def display(self):
+        print("You have %s hours left in the day." % str(self.current_time))
+        print("Day "+str(self.day))
+t = time(8, 0)
+
+class lemonade_storage:
+    def __init__(self, stored, storage):
+        self.stored = stored
+        self.storage = storage
+    def buy(self, amount_bought):
+        global money
+        if self.stored + amount_bought * 10 > self.storage:
+            print("You can't store that much lemonade!")
+        elif money < 3 * amount_bought:
+            print("You don't have enough money!")
+        else:
+            self.stored += amount_bought * 10
+            money -= 3 * amount_bought
+            t.tick()
+    def sell(self, how_long):
+        global money
+        for i in range(how_long):
+            if self.stored > 0:
+                for i in range(randint(1, 5)):
+                    if self.stored > 0:
+                        self.stored -= 1
+                        money += 1
+                    else:
+                        break
+            else:
+                print("You don't have more any lemonade!")
+                break
+            t.tick()
+l = lemonade_storage(0, 100)
+
 def display_stats():
     print("")
-    print("Day: "+str(day))
-    print("Cups of Lemonade: "+str(lemonade))
+    t.display()
+    print("Cups of Lemonade: "+str(l.stored))
     print("Money: "+str(money))
-    print("You have %s hours remaining in the day." % str(round(8 - current_time, 1)))
     print("")
-
-def buy_lemonade(amount):
-    global lemonade
-    global money
-    global current_time
-    global day
-    if money >= 3 * amount:
-        lemonade += 10 * amount
-        money -= 3 *amount
-        sleep(1)
-        current_time += 1
-        display_stats()
-    else:
-        print("")
-        print("You don't have enough money!")
-        print("")
-
-def sell_lemonade():
-    global lemonade
-    global money
-    global current_time
-    global day
-    if lemonade > 0:
-        sell_time = randint(1, 10)/10
-        sell_time = round(sell_time, 1)
-        sleep(sell_time)
-        current_time += sell_time
-        current_time = round(current_time, 1)
-        lemonade -= 1
-        money += 1
-        display_stats()
-    else:
-        print("")
-        print("You don't have any lemonade!")
-        print("")
-
-def check_time():
-    global current_time
-    global day
-    if current_time >= 8:
-        print("Day is done!")
-        current_time = 0
-        day += 1
 
 
 #main game loop
-print("Welcome to Lemonade Empire!  Type help for a list of commands.")
 while True:
     command = input("")
 
@@ -73,25 +69,24 @@ while True:
         display_stats -> displays how much money, lemonade, etc. you have
         exit -> quits the game
         ''')
-    if command == "buy_lemonade":
-        buy_amount = input("How many bottles?(1 bottle = 10 cups): ")
-        try:
-            buy_amount = int(buy_amount)
-            buy_lemonade(buy_amount)
-        except:
-            print("Enter a number next time!")
-    if command == "sell_lemonade":
-        sell_amount = input("How much do you want to sell?: ")
-        try:
-            sell_amount = int(sell_amount)
-            for i in range(sell_amount):
-                sell_lemonade()
-                check_time()
-            print("Done")
-        except:
-            print("Enter a number next time!")
     if command == "exit":
         break
     if command == "display_stats":
         display_stats()
-    check_time()
+
+    if command == "buy_lemonade":
+        amount = input("How many bottles?(1 bottle = 10 cups):")
+        try:
+            amount = int(amount)
+            l.buy(amount)
+            display_stats()
+        except:
+            print("Enter a number next time!")
+    if command == "sell_lemonade":
+        try:
+            how_long = input("How long do you want to sell for?")
+            how_long = int(how_long)
+            l.sell(how_long)
+            display_stats()
+        except:
+            print("Enter a number next time!")
